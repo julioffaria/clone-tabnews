94e64587cf5da5a254d2171de14107ed842eb66b
Julio Faria <jcesarffaria@gmail.com>
2025-10-25 23:24:41 -0300

    add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
-  const phrase = 'Te amo amor <3'
-  return <h1>{phrase}</h1>
+  const phrase = "Te amo amor <3";
+  return <h1>{phrase}</h1>;
 }
 
-export default Home
+export default Home;
